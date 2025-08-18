d5f1ca2318549b908c780dca0758340254bf44c6
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-08-18 08:52:02 -0400

    add IconImages file to Utilities folder

--- Thankful-List-2025-V2/Thankful-List-2025-V2/Utilities/IconImages.swift
+++ Thankful-List-2025-V2/Thankful-List-2025-V2/Utilities/IconImages.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+enum IconImages: String, CaseIterable, Identifiable {
+    case star = "star.fill"
+    case swirl = "swirl.circle.righthalf.filled.inverse"
+    case circle = "circle.dotted.circle.fill"
+    case person = "person.fill"
+    case sun = "sun.min.fill"
+    case pencil = "pencil.circle"
+    case keyboard = "keyboard"
+    case flag = "flag.pattern.checkered"
+    case chain = "personalhotspot"
+    case walk = "figure.walk"
+    case run = "figure.run"
+    case moon = "moonphase.waxing.gibbous.inverse"
+    case bird = "bird.fill"
+    var id: Self { self }
+}
