bb76b7923221bb86d2674eaef05cca5262ccdc0f
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-08-18 09:12:46 -0400

    add CommonThanksData and CommonThanks model to project

---
+++ Thankful-List-2025-V2/Thankful-List-2025-V2/Models/CommonThanks.swift
@@ -0,0 +1,13 @@
+//
+//  CommonThanks.swift
+//  Thankful-List-2025-V2
+//
+//  Created by Thomas Cowern on 8/18/25.
+//
+
+import Foundation
+
+struct CommonThanksData: Codable {
+    let commonThanks: String
+    let thanksType: [String]
+}
---
+++ Thankful-List-2025-V2/Thankful-List-2025-V2/Views/CommonThanks.swift
@@ -0,0 +1,18 @@
+//
+//  CommonThanks.swift
+//  Thankful-List-2025-V2
+//
+//  Created by Thomas Cowern on 8/18/25.
+//
+
+import SwiftUI
+
+struct CommonThanks: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommonThanks()
+}
