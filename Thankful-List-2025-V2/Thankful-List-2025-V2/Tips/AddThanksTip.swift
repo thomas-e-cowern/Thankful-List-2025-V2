974ecc6bdbd6f71a6e69bb7e72fd9faa088de949
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-08-18 08:52:41 -0400

    add Tips folder to project, add AddThanksTip file to Tips folder

--- Thankful-List-2025-V2/Thankful-List-2025-V2/Tips/AddThanksTip.swift
+++ Thankful-List-2025-V2/Thankful-List-2025-V2/Tips/AddThanksTip.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+import TipKit
+
+struct AddThanksTip: Tip {
+    var title: Text { Text("Click to add a Thanks") }
+    var message: Text? { Text("You can click here to add a Thanks from any view") }
+    var image: Image? { Image(systemName: "pencil.circle") }
+}
