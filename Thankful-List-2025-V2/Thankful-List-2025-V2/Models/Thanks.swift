1d3d4c737b95f870a111dbee21c70201719c9d78
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-08-18 08:47:04 -0400

    update HomeView with frameworks and formatting

---
+++ Thankful-List-2025-V2/Thankful-List-2025-V2/Models/Thanks.swift
@@ -0,0 +1,49 @@
+//
+//  ThanksModel.swift
+//  Thankful-List-2025-V2
+//
+//  Created by Thomas Cowern on 8/18/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@Model
+class Thanks: Identifiable {
+    var id = UUID()
+    var title: String
+    var reason: String
+    var date: Date
+    var isFavorite: Bool
+    var icon: String
+    var color: String
+    
+    @Attribute(.externalStorage) var photo: Data?
+    
+    init(title: String, body: String, date: Date, isFavorite: Bool, icon: String, color: String) {
+        self.title = title
+        self.reason = body
+        self.date = date
+        self.isFavorite = isFavorite
+        self.icon = icon
+        self.color = color
+    }
+    
+    var hexColor: Color {
+        Color(hex: self.color) ?? .blue
+    }
+}
+
+extension Thanks {
+    
+    static var sampleThanks: [Thanks] {
+        [
+            Thanks(title: "My home", body: "I am thankful for the roof over my head", date: Date(), isFavorite: false, icon: "person.fill", color: "#FFA500"),
+            Thanks(title: "My car", body: "My car takes me where I need to go", date: Date(), isFavorite: true, icon: "car.fill", color: "00FF00"),
+            Thanks(title: "My freinds", body: "They help my out", date: Date(), isFavorite: true, icon: "house.fill", color: "0000FF"),
+            Thanks(title: "Food", body: "It nourishes my body", date: Date(), isFavorite: false, icon: "figure.table.tennis.circle.fill", color: "0000FF"),
+            Thanks(title: "TV", body: "It keeps me entertained", date: Date(), isFavorite: false, icon: "figure.american.football", color: "00FF00"),
+            Thanks(title: "Radio", body: "For when the TV doesn't work", date: Date(), isFavorite: true, icon: "figure.baseball.circle.fill", color: "#FFA500")
+        ]
+    }
+}
---
+++ Thankful-List-2025-V2/Thankful-List-2025-V2/Views/HomeView.swift
@@ -0,0 +1,66 @@
+//
+//  HomeView.swift
+//  Thankful-List-2025-V2
+//
+//  Created by Thomas Cowern on 8/18/25.
+//
+
+import SwiftUI
+import TipKit
+
+struct HomeView: View {
+    
+    @Environment(\.modelContext) private var modelContext
+    @State private var path = NavigationPath()
+    
+    let addThanksTip = AddThanksTip()
+    
+    var body: some View {
+        NavigationStack(path: $path) {
+            ZStack {
+                VStack(spacing: 30) {
+                    
+                    Image("Thanks")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200)
+                    
+                    Text("Welcome to the Grateful List!")
+                        .font(.title)
+                        .padding()
+                    
+                    Text("The Grateful List exists to help you keep track of people, places, experiences and things that you are grateful for in your life.")
+                        .padding()
+                }
+            }
+            .navigationDestination(for: Thanks.self, destination: { thanks in
+                EditThanksView(navigationPath: $path, thanks: thanks)
+            })
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "plus")
+                        .onTapGesture {
+                            let newThanks = Thanks(title: "", body: "", date: Date.now, isFavorite: false, icon: IconImages.star.rawValue, color: "#007AFF")
+                            modelContext.insert(newThanks)
+                            path.append(newThanks)
+                        }
+                        .popoverTip(addThanksTip)
+                        .accessibilityHint("This will open up the form to add a new Thanks to your list.")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+        .task {
+            try? Tips.resetDatastore()
+            try? Tips.configure(
+                [
+                    .displayFrequency(.immediate),
+                    .datastoreLocation(.applicationDefault)
+                ]
+            )
+        }
+}
