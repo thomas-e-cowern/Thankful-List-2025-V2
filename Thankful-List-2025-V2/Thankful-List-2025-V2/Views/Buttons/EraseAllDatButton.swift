6b19fb0a4a9cf1a39f23850014b3e13e30bea07a
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-08-27 08:16:24 -0400

    add Buttons folder, add EraseAllDatButton

---
+++ Thankful-List-2025-V2/Thankful-List-2025-V2/Views/Buttons/EraseAllDatButton.swift
@@ -0,0 +1,30 @@
+//
+//  EraseAllDatButton.swift
+//  Thankful-List-2025-V2
+//
+//  Created by Thomas Cowern on 8/27/25.
+//
+
+import SwiftUI
+
+struct EraseAllDatButton: View {
+    
+    @Binding var showAlert: Bool
+
+        var body: some View {
+            Button(role: .destructive) {
+                showAlert.toggle()
+            } label: {
+                Label("Erase Data", systemImage: "trash")
+                    .font(.body.weight(.semibold))
+                    .frame(maxWidth: .infinity) // expands to fill width
+            }
+            .buttonStyle(.borderedProminent)
+            .accessibilityLabel("Erase all data")
+            .accessibilityHint("Shows a confirmation alert. This action cannot be undone.")
+        }
+}
+
+#Preview {
+    EraseAllDatButton(showAlert: .constant(false))
+}
